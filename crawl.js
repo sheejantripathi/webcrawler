34df5ac428818c7f073519aa9fe4a29a71023b61
sheejantripathi <sheejantripathi101@gmail.com>
2024-02-03 20:16:03 +0100

    crawl page recursive

--- crawl.js
+++ crawl.js
@@ -1,26 +1,48 @@
 const jsdom = require('jsdom');
 const {JSDOM} = jsdom
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
+    const baseURLObj = new URL(baseURL)
+    const currentURLObj = new URL(currentURL)
+    if (baseURLObj.hostname !== currentURLObj.hostname) {
+        return pages
+    }
+
+    const normalizedCurrentURL = normalizeURL(currentURL)
+    if(pages[normalizedCurrentURL] > 0) {
+        pages[normalizedCurrentURL]++
+        return pages
+    }
+
+    pages[normalizedCurrentURL] = 1
+
+
     console.log(`Actively crawling: ${currentURL}`)
+
     try {
         const response = await fetch(currentURL);
 
         if(response.status > 399) {
             console.log(`Error in fetch with status code: ${response.status} on page: ${currentURL}`)
-            return
+            return pages
         }
 
         const contentType = response.headers.get("content-type")
         if (!contentType.includes("text/html")) {
             console.log(`Non html response, content type: ${contentType} on page ${currentURL}`)
-            return
+            return pages
         }
-        console.log(await response.text())  
+        const crawledHTMLBody = await response.text()
+        
+        const nextURLs = getURLsFromHTML(crawledHTMLBody, baseURL)
+
+        for (const nextURL of nextURLs) {
+            pages = await crawlPage(baseURL, nextURL, pages)
+        }
     } catch (error) {
         console.log(`Error in fetch: ${error.message}, on page ${currentURL}`)
     }
-    
+    return pages
 }
 
 function normalizeURL(urlString) {
