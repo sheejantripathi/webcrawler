1bbb4f369af1cc4e388f74f1e42264e44fa0f8e5
sheejantripathi <sheejantripathi101@gmail.com>
2024-02-03 15:27:32 +0100

    Normalize urlfunction and it's tests

--- crawl.js
+++ crawl.js
@@ -1,5 +1,10 @@
-function normalizeURL() {
-    return new URL({ toString: () => 'blog.boot.dev/path' });
+function normalizeURL(urlString) {
+    const urlObj = new URL(urlString);
+    const hostPath = `${urlObj.hostname}${urlObj.pathname}`
+    if(hostPath.length > 0 && hostPath.slice(-1) === "/") {
+        return hostPath.slice(0, -1)
+    }
+    return hostPath
 }
 
 module.exports = {
--- crawl.test.js
+++ crawl.test.js
@@ -1,9 +1,30 @@
 const {test, expect} = require('@jest/globals')
 const {normalizeURL} = require('./crawl.js')
 
-test('returns normalized version of the URL as string', () => {
-    expect(normalizeURL('https://blog.boot.dev/path/')).toBe('blog.boot.dev/path')
-    expect(normalizeURL('https://blog.boot.dev/path/')).toBe('blog.boot.dev/path')
-    expect(normalizeURL('https://blog.boot.dev/path/')).toBe('blog.boot.dev/path')
-    expect(normalizeURL('https://blog.boot.dev/path/')).toBe('blog.boot.dev/path')
+test('normalize URL strip the protocols', () => {
+    const input = 'https://blog.boot.dev/path';
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalize URL trim trailing slashes', () => {
+    const input = 'https://blog.boot.dev/path/';
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalize URL remove capitals', () => {
+    const input = 'https://BLOG.boot.dev/path/';
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalize URL remove http', () => {
+    const input = 'http://blog.boot.dev/path/';
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
 })
